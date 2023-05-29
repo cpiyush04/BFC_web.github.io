df6e4fdee2f31dfed142490550f63cb1ac7d1faa
piyush chandra <piyushchandra004@gmail.com>
2023-05-30 00:01:52 +0530

    Sports Club website

---
+++ script.js
@@ -0,0 +1,30 @@
+
+   function openNav(){
+      document.getElementById("myNav").style.width = "100%";
+   }
+
+   function openNav1(){
+      document.getElementById("Nav1").style.width = "100%";
+   }
+
+   function closeNav(){
+      document.getElementById("myNav").style.width = "0%";
+   }
+
+   function closeNav1(){
+      document.getElementById("Nav1").style.width = "0%";
+   }
+
+   function opSearch(){
+      let bar = document.getElementById("mySearch");
+      bar.style.display="flex";
+   }
+
+   function reset(){
+      let bar = document.getElementById("mySearch");
+      bar.style.display="none";
+   }
+
+
+
+
